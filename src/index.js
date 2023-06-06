8e2ac53589a55362f59b086b596014f7ad6bcae6
Eren <ceyhunerenerguven.07@outlook.com>
2023-06-06 11:38:43 +0300

    2.0.1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import Storage from "./dist/Storage.js";
-const storage = new Storage();
+import __StorageError from "./dist/StorageError.js";
+const StorageError = __StorageError.default;
 
-for (let index = 0; index < 100; index++) storage.set(`Index-${index}`, index + 1);
-
-storage.set("A2", 0)
-storage.replaceAllKeys({ value: 0, replaceWith: "A1" });
-
-console.log(storage.get("A1"))
+export { StorageError };
+export default Storage;
