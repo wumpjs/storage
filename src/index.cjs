547519d517a2fde2de1cd69b4fb422b5a8915aec
Davut Özgür Süküti <80099012+davutozgursukuti4531@users.noreply.github.com>
2023-07-03 15:12:31 +0300

    2.3.4

--- src/index.cjs
+++ src/index.cjs
@@ -10,8 +10,8 @@ Object.defineProperty(exports, "StorageError", {
   }
 });
 exports.default = void 0;
-var _Storage = _interopRequireDefault(require("./build/Storage.js"));
-var _StorageError = _interopRequireDefault(require("./build/StorageError.js"));
+var _Storage = _interopRequireDefault(require("./build/Storage.cjs"));
+var _StorageError = _interopRequireDefault(require("./build/StorageError.cjs"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 var _default = _Storage.default;
 exports.default = _default;
